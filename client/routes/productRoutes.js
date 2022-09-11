a8cc4c4af6da38487448338db1eaae3c207369d3
Destiny Erhabor <destinyerhabor6@gmail.com>
2022-09-11 00:56:12 -0400

    set up grpc client

--- client/routes/productRoutes.js
+++ client/routes/productRoutes.js
@@ -3,11 +3,11 @@ const grpcRoutes = require('./grpcRoutes')
 
 const router = express.Router()
 
-router.get("/products", grpcRoutes.listProducts)
+router.get("/products", grpcRoutes.listProducts);
 router.get("/products/:id", grpcRoutes.readProduct);
-router.post("/products", grpcRoutes.createProduct)
+router.post("/products", grpcRoutes.createProduct);
 router.put("/products/:id", grpcRoutes.updateProduct);
 router.delete("/products/:id", grpcRoutes.deleteProduct);
 
 
-module.exports = router
+module.exports = router;
